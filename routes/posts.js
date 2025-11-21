ae9bf8710a783966b491ae86cf208c87ce98a39a
Blessing Omolara Abimbola <babimbola80@gmail.com>
2025-11-21 14:37:32 +0100

    Add posts route for fetching and creating posts

---
+++ routes/posts.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+router.get('/', async (req, res) => {
+  const posts = await Post.find().populate('category');
+  res.json(posts);
+});
+
+router.post('/', async (req, res) => {
+  const newPost = new Post(req.body);
+  await newPost.save();
+  res.json(newPost);
+});
+
+module.exports = router;
